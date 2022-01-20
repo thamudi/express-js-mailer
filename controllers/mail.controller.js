66d790ecd8fab12196853fefed2e356f84abdf89
thamudi <tamim.hamoudi@gmail.com>
2022-01-20 20:56:59 +0200

    dev-terminal

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -9,42 +9,42 @@ require('dotenv').config({ path: './config/.env' });
 const EMAIL = process.env.EMAIL;
 
 function sendMail(request, response) {
-    const participant = request.body;
-    const mailOptions = {
-        from: participant.email,
-        to: EMAIL,
-        subject: `${participant.subject} - From ${participant.firstName} ${participant.lastName}`,
-        template: 'index',
-        context: { participant: participant, received: new Date() },
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            response.json({ status: false });
-        } else {
-            console.log('Email sent: ' + info.response);
-            sendResponse(participant);
-            response.json({ status: true });
-        }
-    });
+  const participant = request.body;
+  const mailOptions = {
+    from: participant.email,
+    to: EMAIL,
+    subject: `${participant.subject} - From ${participant.firstName} ${participant.lastName}`,
+    template: 'index',
+    context: { participant: participant, received: new Date() },
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      response.json({ status: false });
+    } else {
+      console.log('Email sent: ' + info.response);
+      sendResponse(participant);
+      response.json({ status: true });
+    }
+  });
 }
 
 function sendResponse(params) {
-    const mailOptions = {
-        from: EMAIL,
-        to: params.email,
-        subject: `Thank you ${params.firstName} for contacting me`,
-        template: 'response',
-        context: { participant: params },
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            response.json({ status: false });
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+  const mailOptions = {
+    from: EMAIL,
+    to: params.email,
+    subject: `Thank you ${params.firstName} for contacting me`,
+    template: 'response',
+    context: { participant: params },
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      response.json({ status: false });
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
 
 
